81d3b55b9e99a8a8af72b9bab3abec1a73750581
abhi1401 <abhishek.mvr@outlook.com>
2018-02-17 21:53:08 +0530

    initial commit

---
+++ DataServer/PCDetails.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[PCDetails]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(20) NULL, 
+    [IpAddress] NCHAR(20) NULL, 
+    [Affected] INT NULL
+)
+
---
+++ DataServer/RawData.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[RawData]
+(
+	[lanip] NCHAR(20) NOT NULL , 
+    [wanip] NCHAR(20) NOT NULL, 
+    [time] INT NOT NULL, 
+    [port] INT NOT NULL, 
+    [failure] INT NOT NULL
+)
